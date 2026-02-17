77ae1cc609780d3e6da4e3f40cc2dfce10f65a38
Michael Mazyar <taasaa@gmail.com>
2026-02-17 17:09:24 -0500

    Add ,q for :qa and ,w for :wa

--- lua/custom/keymaps.lua
+++ lua/custom/keymaps.lua
@@ -2,3 +2,7 @@
 
 -- Map :Q to :q (prevent accidental shift+q)
 vim.keymap.set({ 'n', 'x' }, 'Q', ':q<CR>', { desc = 'Quit (same as :q)' })
+
+-- Leader-based keymaps
+vim.keymap.set('n', ',q', ':qa<CR>', { desc = 'Quit all' })
+vim.keymap.set('n', ',w', ':wa<CR>', { desc = 'Write all' })
